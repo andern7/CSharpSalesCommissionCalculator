9b10dcff496e7d65a5c987c45ab1a406f01de3db
jhackney01 <jhackney01@att.net>
2014-02-12 11:33:44 -0600

    SalesCommissionCalc

---
+++ SalesCommissionCalculator/SalesCommissionCalcMain.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SalesCommissionCalculator
+{
+    class SalesCommissionCalcMain
+    {
+        static void Main(string[] args)
+        {            
+            double grossSales = 0;
+
+            SalesCommissionCalculator mySales = new SalesCommissionCalculator(
+                grossSales);
+
+            Console.Write("Please input sales for the week or -1 to quit: ");
+            grossSales = double.Parse(Console.ReadLine());
+
+            mySales.Calculations(grossSales);            
+        }
+    }
+}
---
+++ SalesCommissionCalculator/SalesCommissionCalculator.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace SalesCommissionCalculator
+{
+    class SalesCommissionCalculator
+    {
+        public double GrossSales { get; set; }
+
+        public SalesCommissionCalculator(double sales)
+        {
+            GrossSales = sales;
+        }
+
+        public void Calculations(double sales)
+        {            
+            double totalSales = 0;
+            double earnings = 0;
+            const int SALARY = 200;
+            const double PERCENTAGE = 0.09;               
+            
+            while (sales != -1)
+            {
+                totalSales += sales;
+                earnings = SALARY + (totalSales * PERCENTAGE);
+                Console.WriteLine("Weekly earnings: {0:F}", earnings);
+                Console.WriteLine();
+
+                Console.Write("Please input sales for the week or -1 to quit: ");
+                sales = double.Parse(Console.ReadLine());
+            }            
+        }
+    }
+}
